3a62b9302b41af673ab3e299794c856191a16e5c
edmilson <edmilsonribeiro.17072005@gmail.com>
2022-04-08 22:19:04 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int Numero;
+
+Console.WriteLine("Digite um numero: ");
+Numero = Convert.ToInt32 (Console.ReadLine());
+
+if (Numero <0 )
+
+{
+    Console.ForegroundColor=ConsoleColor.Red;
+    Console.WriteLine ("negativo");
+}
+else if (Numero >0 )
+{
+    Console.ForegroundColor=ConsoleColor.Green;
+    Console.WriteLine ("positivo");    
+}
+else
+{
+    Console.ForegroundColor=ConsoleColor.Cyan;
+    Console.WriteLine ("zero");
+}
+
+Console.ResetColor();
